88068c7af483d4637edad1adbebf5b01ba8fd9d3
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2019-02-20 23:02:54 +0000

    added benchmark of new type of mpi wavelet operator

--- cpp/benchmarks/wavelet_operator_mpi.cc
+++ cpp/benchmarks/wavelet_operator_mpi.cc
@@ -79,6 +79,37 @@ class WaveletOperatorMPIFixture : public ::benchmark::Fixture {
   // Get the number of wavelet coefs
   t_uint n_wave_coeff;
 };
+class WaveletOperatorMPIFullFixture : public ::benchmark::Fixture {
+ public:
+  WaveletOperatorMPIFullFixture(){};
+  void SetUp(const ::benchmark::State& state) {
+    m_imsizex = state.range(0);
+    m_imsizey = state.range(0);
+    b_utilities::update_comm(m_world);
+    sopt::wavelets::SARA_MPI m_sara{
+        std::make_tuple("Dirac", 3u, m_world), std::make_tuple("DB1", 3u, m_world),
+        std::make_tuple("DB2", 3u, m_world),   std::make_tuple("DB3", 3u, m_world),
+        std::make_tuple("DB4", 3u, m_world),   std::make_tuple("DB5", 3u, m_world),
+        std::make_tuple("DB6", 3u, m_world),   std::make_tuple("DB7", 3u, m_world),
+        std::make_tuple("DB8", 3u, m_world)};
+
+    // Get the number of wavelet coefs
+    n_wave_coeff = m_sara.size() * m_imsizey * m_imsizex;
+    m_Psi = sopt::linear_transform<t_complex>(m_sara, m_imsizey, m_imsizex);
+  }
+
+  void TearDown(const ::benchmark::State& state) {}
+
+  // A bunch of useful variables
+  t_uint m_counter;
+  // MPI communicator
+  sopt::mpi::Communicator m_world;
+  sopt::LinearTransform<Vector<t_complex>> m_Psi = sopt::linear_transform_identity<t_complex>();
+  t_uint m_imsizex;
+  t_uint m_imsizey;
+  // Get the number of wavelet coefs
+  t_uint n_wave_coeff;
+};
 
 BENCHMARK_DEFINE_F(WaveletOperatorMPIFixture, Forward)(benchmark::State& state) {
   // Benchmark the application of the operator
@@ -107,6 +138,33 @@ BENCHMARK_DEFINE_F(WaveletOperatorMPIFixture, Adjoint)(benchmark::State& state) 
     state.SetIterationTime(b_utilities::duration(start, end, m_world));
   }
 }
+BENCHMARK_DEFINE_F(WaveletOperatorMPIFullFixture, Forward)(benchmark::State& state) {
+  // Benchmark the application of the operator
+
+  // Apply Psi to a temporary vector
+  Vector<t_complex> image = Vector<t_complex>::Random(m_imsizey * m_imsizex);
+  Vector<t_complex> const wavelet_coeff = Vector<t_complex>::Ones(n_wave_coeff);
+
+  while (m_world.broadcast<int>(state.KeepRunning())) {
+    auto start = std::chrono::high_resolution_clock::now();
+    image = m_Psi * wavelet_coeff;
+    auto end = std::chrono::high_resolution_clock::now();
+    state.SetIterationTime(b_utilities::duration(start, end, m_world));
+  }
+}
+
+BENCHMARK_DEFINE_F(WaveletOperatorMPIFullFixture, Adjoint)(benchmark::State& state) {
+  // Apply Psi to a temporary vector
+  Vector<t_complex> const image = Vector<t_complex>::Ones(m_imsizey * m_imsizex);
+  Vector<t_complex> wavelet_coeff = Vector<t_complex>::Zero(n_wave_coeff);
+
+  while (m_world.broadcast<int>(state.KeepRunning())) {
+    auto start = std::chrono::high_resolution_clock::now();
+    wavelet_coeff = m_Psi.adjoint() * image;
+    auto end = std::chrono::high_resolution_clock::now();
+    state.SetIterationTime(b_utilities::duration(start, end, m_world));
+  }
+}
 
 BENCHMARK_REGISTER_F(WaveletOperatorMPIFixture, Forward)
     // //->Apply(b_utilities::Arguments)
@@ -117,7 +175,25 @@ BENCHMARK_REGISTER_F(WaveletOperatorMPIFixture, Forward)
     ->ReportAggregatesOnly(true)
     ->Unit(benchmark::kMillisecond);
 
+BENCHMARK_REGISTER_F(WaveletOperatorMPIFullFixture, Forward)
+    // //->Apply(b_utilities::Arguments)
+    ->RangeMultiplier(2)
+    ->Range(128, 128 << 3)
+    ->UseManualTime()
+    ->Repetitions(5)
+    ->ReportAggregatesOnly(true)
+    ->Unit(benchmark::kMillisecond);
+
 BENCHMARK_REGISTER_F(WaveletOperatorMPIFixture, Adjoint)
+    // //->Apply(b_utilities::Arguments)
+    ->RangeMultiplier(2)
+    ->Range(128, 128 << 3)
+    ->UseManualTime()
+    ->Repetitions(5)
+    ->ReportAggregatesOnly(true)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK_REGISTER_F(WaveletOperatorMPIFullFixture, Adjoint)
     // //->Apply(b_utilities::Arguments)
     ->RangeMultiplier(2)
     ->Range(128, 128 << 3)
