1186fcf45a5c657fea0fc1e840173e8103230074
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2019-05-12 01:03:56 +0100

    removed saving new image

--- cpp/tests/algo_factory.cc
+++ cpp/tests/algo_factory.cc
@@ -184,7 +184,7 @@ TEST_CASE("joint_map_factory") {
   auto const diagnostic = joint_map();
   //  CHECK(diagnostic.reg_niters == 13);
   const Image<t_complex> image = Image<t_complex>::Map(diagnostic.x.data(), imsizey, imsizex);
-  pfitsio::write2d(image.real(), expected_solution_path);
+  //pfitsio::write2d(image.real(), expected_solution_path);
   CAPTURE(Vector<t_complex>::Map(solution.data(), solution.size()).real().head(10));
   CAPTURE(Vector<t_complex>::Map(image.data(), image.size()).real().head(10));
   CAPTURE(Vector<t_complex>::Map((image / solution).eval().data(), image.size()).real().head(10));
@@ -193,7 +193,7 @@ TEST_CASE("joint_map_factory") {
   const Vector<t_complex> residuals = measurements_transform->adjoint() *
                                       (uv_data.vis - ((*measurements_transform) * diagnostic.x));
   const Image<t_complex> residual_image = Image<t_complex>::Map(residuals.data(), imsizey, imsizex);
-  pfitsio::write2d(residual_image.real(), expected_residual_path);
+  //pfitsio::write2d(residual_image.real(), expected_residual_path);
   CAPTURE(Vector<t_complex>::Map(residual.data(), residual.size()).real().head(10));
   CAPTURE(Vector<t_complex>::Map(residuals.data(), residuals.size()).real().head(10));
   CHECK(residual_image.real().isApprox(residual.real(), 1e-6));
