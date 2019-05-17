bb58d1a5f8289339c5d5acbf86b38bd988af7934
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2019-05-17 21:27:42 +0100

    went back to old calculation for alpha

--- cpp/main.cc
+++ cpp/main.cc
@@ -537,7 +537,19 @@ int main(int argc, const char **argv) {
 #endif
     const t_real beta_param = 1. / 3;
     const t_real alpha_param =
-        1. + 0.5 * beta_param * (l1_norm(measurements_transform->adjoint() * uv_data.vis));
+        1. + 0.5 * beta_param *
+                 (
+#ifdef PURIFY_MPI
+                     (using_mpi) ? world.all_reduce(utilities::step_size<Vector<t_complex>>(
+                                                        uv_data.vis, measurements_transform,
+                                                        wavelets_transform, sara_size),
+                                                    MPI_MAX)
+                                 :
+#endif
+                                 (wavelets_transform->adjoint() *
+                                  (measurements_transform->adjoint() * uv_data.vis).eval())
+                                     .cwiseAbs()
+                                     .maxCoeff());
     auto const joint_map =
         sopt::algorithm::JointMAP<sopt::algorithm::ImagingForwardBackward<t_complex>>(
             fb, l1_norm, params.height() * params.width() * sara_size)
